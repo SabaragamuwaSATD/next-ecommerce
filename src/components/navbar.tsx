296991d1fb344ec0c9d5eb9cc69460da8fb0f4cc
Sabaragamuwa S.A.T.D-IT21836268 <it21836268@my.sliit.lk>
2024-06-21 00:51:30 +0530

    corrected some issues

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
-  );
+  return <div className="">Navbar</div>;
 };
 
 export default Navbar;
